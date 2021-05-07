62a0e2d4f4269e56b704398834289c6d067ed6c0
Felipe Monsão <46978878+femonsao@users.noreply.github.com>
2021-05-06 22:33:10 -0300

    teste

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+@extends('templates/crudTemplate')
+
+@section('content')
+<h1 class='text-center'>Crud simples</h1>
+
+<div class="text-center">
+     
+    <a href="{{ url('user/create') }}">
+      <button class="btn btn-success">Cadastrar</button>
+
+    </a>
+
+</div>
+
+<div class="col-8 m-auto mt-3 mb-4">
+    @csrf
+  <table class="table texte-center">
+    <thead class="thead-dark " style="background-color: #2d2424;
+      color: white;">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col " class='thAction'>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($user as $users)
+    <tr>
+        <th scope="row">{{$users->id}}</th>
+        <td>{{$users->name}}</td>
+        <td class="actionsBtn">
+        
+          <a href="{{url("user/$users->id/edit")}}">
+            <button class="btn btn-primary">Editar</button>
+          
+          </a>
+        
+          <a href="{{url("user/$users->id")}}" >
+            <button class="btn btn-danger">Excluir</button>
+          </a>
+        </td>
+
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+
+</div>
+@endsection
